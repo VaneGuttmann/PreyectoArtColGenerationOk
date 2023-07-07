74ea6b49a1f5be64c8048b7c3aad35f92c85017e
VaneGuttmann <132688045+VaneGuttmann@users.noreply.github.com>
2023-07-07 16:37:45 -0500

    DB_ArtCol_Workbench tarea 11

---
+++ Workbench_ArtCol/ArtColOk_cod_whitData.sql
@@ -0,0 +1,14 @@
+INSERT INTO pelicula (idPelicula, Nombre, Descripcion, Año, Duracion, Reparto, Director, id_lista, Lista_reproduccion_idLista_reproduccion, Lista_reproduccion_Usuario_idUsuarios, Categoria_idCategoria)
+VALUES ('1', "Se nos armo la gorda", "Lorem ipsum dolor sit amet consectetur adipisicing elit", '2016', '01:45', "Pablo Perez, Francisco Rojas, Pepita Suarez", "Diego Rojas", '1','1','1','1'); 
+
+INSERT INTO pelicula (idPelicula, Nombre, Descripcion, Año, Duracion, Reparto, Director, id_lista, Lista_reproduccion_idLista_reproduccion, Lista_reproduccion_Usuario_idUsuarios, Categoria_idCategoria)
+VALUES ('2', "Rojo Red", "Lorem ipsum dolor sit amet consectetur adipisicing elit", '2018', '00:20', "Pepito Perez, Daniel Suarez", "Andrea Marin", '2','2','2','2');
+
+INSERT INTO pelicula (idPelicula, Nombre, Descripcion, Año, Duracion, Reparto, Director, id_lista, Lista_reproduccion_idLista_reproduccion, Lista_reproduccion_Usuario_idUsuarios, Categoria_idCategoria)
+VALUES ('3', "La pelota de letras", "Lorem ipsum dolor sit amet consectetur adipisicing elit", '2015', '01:23', "Andres Lopez", "Andres Lopez", '3','3','3','3');
+
+INSERT INTO pelicula (idPelicula, Nombre, Descripcion, Año, Duracion, Reparto, Director, id_lista, Lista_reproduccion_idLista_reproduccion, Lista_reproduccion_Usuario_idUsuarios, Categoria_idCategoria)
+VALUES ('4', "Virus tropical", "Lorem ipsum dolor sit amet consectetur adipisicing elit", '2021', '01:15', "Andres Perez, Daniela Rojas, Pepita Suarez", "Juan Rozo", '4','4','4','4');
+
+INSERT INTO pelicula (idPelicula, Nombre, Descripcion, Año, Duracion, Reparto, Director, id_lista, Lista_reproduccion_idLista_reproduccion, Lista_reproduccion_Usuario_idUsuarios, Categoria_idCategoria)
+VALUES ('5', "Revel love", "Lorem ipsum dolor sit amet consectetur adipisicing elit", '2019', '00:35', "Pablo Perez, Francisco Rojas, Pepita Suarez", "Diego Rojas", '5','5','5','5');
